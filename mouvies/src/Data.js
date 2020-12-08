0e639afa85b8c444046c964b692e7dacb1804cec
ahmed aouadi <ahmedaouadi86@gmail.com>
2020-12-08 20:03:54 +0100

    first commit

---
+++ mouvies/src/Data.js
@@ -0,0 +1,94 @@
+export const moviesData = [
+    {
+      id: Math.random(),
+      image: "Arab Blues.jpg",
+      rating: 5,
+      name: "Arab Blues",
+      date: "April 2017",
+    },
+    
+    {
+      id: Math.random(),
+      image:"Dachra.jpg",
+      rating: 4,
+      name: "Dachra",
+      date: "octobre 2020",
+    },
+    {
+      id: Math.random(),
+      image: "Day of the falcon.jpg",
+      rating: 2,
+      name: "Day of the falcon",
+      date: "November 2012",
+    },
+    {
+      id: Math.random(),
+      image:  "Fleur d'oubli.jpg",
+      rating: 3,
+      name: "Fleur d'oubli",
+      date: "mai 2018",
+    },
+    {
+      id: Math.random(),
+      image:   "Hedi.jpg",
+      rating: 5,
+      name: "Fleur d'oubli",
+      date: "juillet 2019",
+    },
+  {
+      id: Math.random(),
+      image: "Le professeur.jpg",
+      rating: 2,
+      name: "Le professeur",
+      date: "mai 2018",
+    },
+    {
+      id: Math.random(),
+      image: "Nour.jpg",
+      rating: 3,
+      name: "Nour",
+      date: "juillet 2017",
+    },
+
+    {
+      id: Math.random(),
+      image: "Brotherhood.jpg",
+      rating: 1,
+      name: "brotherhood",
+      date: "mars 2019",
+    },
+    {
+      id: Math.random(),
+      image: "Corps etranger.jpg",
+      rating: 3,
+      name: "corps etranger",
+      date: "juin 2019",
+    },
+    {
+      id: Math.random(),
+      image: "Printemp tunisien.jpg",
+      rating: 4,
+      name: "Printemp tunisien",
+      date: "october 2016",
+    },
+    {
+      id: Math.random(),
+      image:  "War reporter.jpg",
+      rating: 2,
+      name: "War reporter",
+      date: "novembre 2018",
+    },
+   
+    {
+      id: Math.random(),
+      image:  "Ala Kaf ifrit.jpg",
+      rating: 4,
+      name: "Ala kaf ifrit",
+      date: "avril 2015",
+    },
+   
+   
+
+   
+   
+  ];
---
+++ mouvies/src/components/Search.js
@@ -0,0 +1,21 @@
+import React from "react";
+import StarRating from "./StarRating";
+
+const Search = ({ rate, search, handleSearch, handleRating }) => {
+  return (
+    <div className="header-container">
+      <h1>Tunisian movies</h1>
+      <div className="search-container">
+        <input
+          onChange={(event) => handleSearch(event)}
+          value={search}
+          type="text"
+          placeholder="search for a movie ....."
+        />
+        <StarRating movieRating={rate} handleRating={handleRating} />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
